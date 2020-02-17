f8b1af38d0d5df158ab5723a1c97786492a53812
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-02-17 15:45:45 +0100

    JS basket move functionality begun

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,36 @@
 (function() {
     console.log('script.js is connected');
+
+    const basket = {
+        el: document.getElementById('player'),
+        x: 0,
+        y: 0,
+        move() {
+        this.el.style.transform = `translate(${this.x}px, ${this.y}px)`;
+        }
+    };
+    
+    const keyPress = {
+        fn(e) {
+            const x = e.which;
+            if (x >= 37 && x <= 40) {
+                e.preventDefault();
+                keyPress[x] = e.type === "keydown"; 
+            }
+        }
+    };
+    
+    const update = () => {
+        if (keyPress[37]) basket.x -= 2;
+        if (keyPress[39]) basket.x += 2;
+        basket.move();
+    }
+    
+    document.addEventListener('keydown', keyPress.fn);
+    document.addEventListener('keyup', keyPress.fn);
+    
+    (function init() {
+        update();
+        window.requestAnimationFrame(init);
+    })();
 })();
