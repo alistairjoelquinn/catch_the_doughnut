193ca4535de6fbb29faeb68f99d499dd4c8012b7
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-02-17 15:17:43 +0100

    further routing

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('./public'));
+
+app.get('/*', (req, res) => {
+    res.redirect('/');
+});
+
+app.listen(8080, () => console.log('server listening'));
