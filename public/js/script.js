ca6e76a91cfcf86b4bc0666ba268ab057959fecf
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-02-17 17:28:42 +0100

    further variable improvements in scriptjs

--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,8 @@
     const basket = {
         target: document.getElementById('player'),
         x: 0,
-        y: 0,
         move() {
-        this.target.style.transform = `translate(${this.x}px, ${this.y}px)`;
+            this.target.style.transform = `translate(${this.x}px)`;
         }
     };
     
@@ -16,8 +15,12 @@
     };
     
     const init = () => {
-        if (keyPress[37]) basket.x -= 2;
-        if (keyPress[39]) basket.x += 2;
+        if (keyPress[37]) {
+            basket.x -= 2;
+        }
+        if (keyPress[39]) {
+            basket.x += 2;
+        }
         basket.move();
         requestAnimationFrame(init);
     }
