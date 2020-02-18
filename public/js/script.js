7a1d8d070a840b6eb6f90fdbf2493a3bd2278cd3
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-02-18 14:21:16 +0100

    improved window response

--- public/js/script.js
+++ public/js/script.js
@@ -16,35 +16,45 @@
             keyPress[e.keyCode] = e.type === "keydown"; 
         }
     };
-    
-    const init = () => {
-        scoreBox.innerText = score;
+
+    const foodMove = () => {
         let foods = $('section>div');
         for(let x = 0; x < foods.length; x++) {
-            newHeight = parseInt(((foods.eq(x).css('top')).split('px')[0]), 10) + 2;
+            newHeight = parseInt(((foods.eq(x).css('top')).split('px')[0]), 10) + 4;
             itemLeft = foods.eq(x).offset().left;
             if(((newHeight > (window.innerHeight - 100))) && itemLeft > basket.x && itemLeft < (basket.x + 80) && foods.eq(x).text() == '🍩') {
                 score++;
                 foods.eq(x).remove();
+                scoreBox.innerText = score;
             }
-            foods.eq(x).css({
-                top: `${newHeight}px`
-            });
+            foods.eq(x).css({top: `${newHeight}px`});
             if(newHeight > window.innerHeight) {
                 foods.eq(x).remove();
             };
         }
-        if (keyPress[37]) basket.x -= 4;
-        if (keyPress[39]) basket.x += 4;
-        basket.move();
+    }
+
+    const makeFood = () => {
         if(Math.random() > 0.96) {
             let elem = document.createElement('div');
             let text = food[Math.floor(Math.random() * 24)];
             elem.innerText = text;
             elem.style.top = '-60px';
-            elem.style.transform = `translateX(${Math.random()*1200}px)`;
+            elem.style.transform = `translateX(${Math.random() * window.innerWidth}px)`;
             document.getElementById('waiting').appendChild(elem);
         }
+    }
+
+    const basketCheck = () => {
+        if (keyPress[37]) basket.x -= 4;
+        if (keyPress[39]) basket.x += 4;
+    }
+    
+    const init = () => {
+        foodMove();
+        basketCheck();
+        basket.move();
+        makeFood();
         requestAnimationFrame(init);
     }
     
